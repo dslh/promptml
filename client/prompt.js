742356847febf46e494206c068201bee07b3f962
doug <d.lakehammond@gmail.com>
2012-02-05 21:48:43 +1300

    Fixed prompt for more than one argument.

--- client/prompt.js
+++ client/prompt.js
@@ -2,7 +2,7 @@ $('#prompt').submit(function(event) {
   event.preventDefault();
 
   var input = $('#input');
-  var cmd = input.val().replace(' ','+');
+  var cmd = input.val().replace(/ /g,'+');
   input.val('');
   $.get('/cmd?' + escape(cmd), function (data) {
     $('#output').append('<li>' + data + '</li>');
