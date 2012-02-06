945038df0a4088172a22a35766cc6a95f313869b
doug <d.lakehammond@gmail.com>
2012-02-06 16:59:39 +1300

    Cleaning up javascript a bit.

--- client/prompt.js
+++ client/prompt.js
@@ -2,9 +2,16 @@ $('#prompt').submit(function(event) {
   event.preventDefault();
 
   var input = $('#input');
-  var cmd = input.val().replace(/ /g,'+');
-  input.val('');
-  $.get('/cmd?' + escape(cmd), function (data) {
-    $('#output').append('<li>' + data + '</li>');
+  var cmd = $.trim(input.val());
+  if (cmd.length == 0) return;
+
+  $.get('/cmd?' + escapeCommand(cmd), function (response) {
+    $('#output').append(
+            '<li><div class="command">' + cmd + '</div>' +
+            '<div class="response">' + response + '</div></li>');
   });
 });
+
+function escapeCommand(cmd) {
+  return escape(cmd.replace(/ /g,'+'));
+}
