e07f61eaa3d4df9a6d536449f27854386c9c5776
doug <d.lakehammond@gmail.com>
2012-02-07 22:17:55 +1300

    Sleep action for testing slow response times.

---
+++ lib/sleep.rb
@@ -0,0 +1,24 @@
+# Action that sleeps for a number of seconds.
+# Not a particularly useful one this, but it
+# helps with testing.
+require File.dirname(__FILE__) + '/trollop_action.rb'
+
+class Sleep < TrollopAction
+  def initialize
+    super do
+      banner <<-EOS
+Usage: sleep <seconds>
+Waits the specified number of seconds and
+then exits.
+EOS
+    end
+  end
+
+  def action env, cmd, opts, args
+    cl_error 'Duration not supplied.' if args.empty?
+    cl_error 'Too many arguments' if args.length > 1
+    cl_error "Invalid duration '#{args[0]}'" unless /^\d+$/ =~ args[0]
+
+    sleep args[0].to_i
+  end
+end
