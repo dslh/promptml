fac9d29f558aa561312f7e1c8d9f1ffd68eefc9f
doug <d.lakehammond@gmail.com>
2012-02-12 19:45:34 +1300

    Tab completion service.

---
+++ lib/promptml/tab_completion.rb
@@ -0,0 +1,102 @@
+require 'rack/request'
+require 'rack/response'
+require "#{File.dirname __FILE__}/paths.rb"
+
+module PrompTML
+
+  # Provides a tab completion service for both commands and files.
+  # Expects GET requests of the form:
+  #   /path/to/tab/completion?(cmd|file)&<root>
+  #
+  # TabCompletion will return four types of result:
+  #  - When there is only one match for the given root, it will
+  #    contain only the match, with a trailing space to denote
+  #    that it is an exact match.
+  #  - When the root given is ambiguous, it will be an HTML
+  #    list of potential matches.
+  #  - When the given root is part of a longer root common to
+  #    all matches, it will contain only the longer root with
+  #    no trailing space.
+  #  - When there are no matches a short informational HTML
+  #    message will be produced.
+  class TabCompletion
+    def initialize commands
+      raise TypeError unless commands.public_methods.include? :find_all
+      @commands = commands
+    end
+
+    def call env
+      request = Rack::Request.new(env)
+      unless request.query_string =~ /^(cmd|file)(&[^&]*)?$/
+        return [400, {}, ['bad query string']]
+      end
+      type, root = request.query_string.split '&'
+      cwd = Paths.cwd env
+      root ||= ''
+
+      matches = if type == 'cmd'
+        matching_commands root
+      else
+        matching_files root, cwd
+      end
+      return not_found root if matches.empty?
+      return single_match matches[0] if matches.size == 1
+
+      common = common_root_of matches
+      return Rack::Response.new common if root != common
+      return match_list type, matches
+    end
+
+    def matching_commands root
+      @commands.select { |cmd| root? cmd, root }
+    end
+
+    def matching_files root, cwd
+      root = './' if root.empty?
+      absolute = Paths.absolute? root
+      post_slash = root[-1] == '/'
+      root = Paths.make_absolute root, cwd unless absolute
+      root = root.chomp('/') + '/' if post_slash
+      root = root + '*' unless root[-1] == '*'
+      Paths[root].collect do |match|
+        directory = Paths.directory? match
+        match = Paths.make_relative! cwd, match unless absolute
+        match << '/' if directory
+        match
+      end
+    end
+
+    def common_root_of matches
+      root = matches[0]
+      matches.each do |match|
+        root = root[0...-1] until root? match, root
+      end
+      root
+    end
+
+    def single_match match
+      match << ' ' unless match[-1] == '/'
+      Rack::Response.new match
+    end
+
+    def match_list type, matches
+      Rack::Response.new <<-EOS
+<ul class='#{type} matches'>
+  <li>#{matches.sort.join("</li><li>")}</li>
+</ul>
+EOS
+    end
+
+    def not_found root
+      Rack::Response.new <<-EOS
+<span class='error'>
+  Nothing matches <code>#{root}</code>
+</span>
+EOS
+    end
+
+    def root? str, root
+      str.index(root) == 0
+    end
+  end
+end
---
+++ spec/tab_completion_spec.rb
@@ -0,0 +1,117 @@
+require 'promptml/tab_completion'
+require 'promptml/paths'
+require 'rack/mock'
+require 'rack/contrib/cookies'
+
+module PrompTML
+
+  describe TabCompletion do
+    before(:all) do
+      Paths.root = 'spec/test_fs'
+    end
+
+    before(:each) do
+      @app = TabCompletion.new ['foo','bar','baz']
+      @app = Rack::Cookies.new(@app)
+      @request = Rack::MockRequest.new(@app)
+      @opts = { 'HTTP_COOKIE' => 'CWD=%2Ffolder' }
+    end
+
+    it "should be constructed with a list of commands" do
+      TabCompletion.new ['a','b','c']
+      expect { TabCompletion.new }.to raise_error
+      expect { TabCompletion.new 'a duck' }.to raise_error
+    end
+
+    it "should return the command matching the given root" do
+      response = @request.get('/tab?cmd&f', @opts)
+      response.status.should == 200
+
+      # A trailing space denotes an exact match
+      response.body.should == 'foo '
+    end
+
+    it "should list all matches for ambiguous roots" do
+      response = @request.get('/tab?cmd&ba', @opts)
+      response.status.should == 200
+
+      # A disambiguation list may include arbitrary html
+      # but must include all matches.
+      response.body.should match 'bar'
+      response.body.should match 'baz'
+    end
+
+    it "should produce the longest common root for ambiguities" do
+      response = @request.get('/tab?cmd&b', @opts)
+
+      # No trailing space indicates an ambiguous root
+      response.body.should == 'ba'
+    end
+
+    it "should give a full command listing when no root is given" do
+      response = @request.get('/tab?cmd', @opts)
+      response.body.should match 'foo'
+      response.body.should match 'bar'
+      response.body.should match 'baz'
+    end
+
+    it "should match absolute paths" do
+      response = @request.get('/tab?file&/file_1.', @opts)
+      response.body.should == '/file_1.txt '
+
+      response = @request.get('/tab?file&/file_', @opts)
+      response.body.should match 'file_1.txt'
+      response.body.should match 'file_2.txt'
+
+      response = @request.get('/tab?file&/folder/file_a.t', @opts)
+      response.body.should == '/folder/file_a.txt '
+    end
+
+    it "should match relative paths" do
+      response = @request.get('/tab?file&file_a.', @opts)
+      response.body.should == 'file_a.txt '
+
+      response = @request.get('/tab?file&file_', @opts)
+      response.body.should match 'file_a.txt'
+      response.body.should match 'file_b.txt'
+
+      response = @request.get('/tab?file&fi', @opts)
+      response.body.should == 'file_'
+    end
+
+    it "should match directories with a trailing slash" do
+      response = @request.get('/tab?file&s', @opts)
+      response.body.should == 'subfolder/'
+
+      response = @request.get('/tab?file&/f', @opts)
+      response.body.should match '/folder/'
+      response.body.should match '/file_1.txt'
+    end
+
+    it "should list whole directories" do
+      response = @request.get('/tab?file', @opts)
+      response.body.should match 'subfolder/'
+      response.body.should match 'file_a.txt'
+      response.body.should match 'file_b.txt'
+
+      response = @request.get('/tab?file&/', @opts)
+      response.body.should == '/f'
+
+      response = @request.get('/tab?file&subfolder/', @opts)
+      response.body.should == 'subfolder/one_file.txt '
+    end
+
+    it "should supply a message if no result is found" do
+      expected_text = "Nothing matches"
+      response = @request.get('/tab?cmd&far', @opts)
+      response.body.should match expected_text
+
+      response = @request.get('/tab?file&doesnt/exist', @opts)
+      response.body.should match expected_text
+
+      response = @request.get('/tab?file&/doesnt/exist', @opts)
+      response.body.should match expected_text
+    end
+  end
+
+end
