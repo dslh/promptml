163a5aa9a62b4ae691a3df8c38df9b060ac82d29
Doug Hammond <d.lakehammond@gmail.com>
2012-02-23 18:50:42 +1300

    show command can now also show text files.

--- lib/promptml/show.rb
+++ lib/promptml/show.rb
@@ -1,4 +1,6 @@
+require 'rubygems'
 require 'stringio'
+require 'coderay'
 require "#{File.dirname __FILE__}/paths.rb"
 
 module PrompTML
@@ -7,6 +9,13 @@ module PrompTML
   # document(s) in the output. So far it
   # only supports images.
   class Show
+    IMAGE_EXTENSIONS = ['.jpg','.jpeg','.gif','.png','.webp']
+    class << self
+      def image? file
+        IMAGE_EXTENSIONS.include? File.extname(file)
+      end
+    end
+
     def call env, args
       args.shift
       output = StringIO.new
@@ -15,11 +24,23 @@ module PrompTML
       args.each do |pattern|
         pattern = Paths.make_absolute pattern, cwd
         Paths[pattern].each do |file|
-          output << "<img src='/client#{file}' title='#{file}'/>"
+          output << show_file(file)
         end
       end
 
       output.string
     end
+
+    def show_file file
+      <<-EOS
+<div class='file'>
+  <span class='file_name'>#{File.basename file}</span>
+  #{Show.image?(file) ?
+      "<img src='/client#{file}' title='#{file}'/>" :
+      CodeRay.scan_file(Paths.real_path file).div}
+</div>
+EOS
+    end
+
   end
 end
