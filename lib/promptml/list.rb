6db5d4cea5988d81ca566357715ff234dc07ea4e
doug <d.lakehammond@gmail.com>
2012-02-23 20:04:45 +1300

    Reworking the style of `ls` output.

--- lib/promptml/list.rb
+++ lib/promptml/list.rb
@@ -12,20 +12,34 @@ module PrompTML
       args.shift
       cwd = env['rack.cookies']['CWD']
       output = StringIO.new
-      output << '<ul class="file_list">'
+      output << "<div class='file_list'>"
 
       args = ['*'] if args.empty?
       args.each do |pattern|
         pattern = Paths.make_absolute pattern, cwd
         Paths[pattern].each do |file|
           type = Paths.directory?(file) ? 'directory' : 'file'
-          output << "<li class='#{type}'>#{File.basename file}</li>"
+          output << item(file, type)
         end
       end
       
-      output << '</ul>'
+      output << "</div>"
       output.string
     end
+
+    # returns html for a list item with a link
+    # to the file.
+    def item file, type
+      <<-EOS
+  <a href='javascript:promptml("#{
+    type == 'file' ? 'show' : 'cd'
+  } #{file}")'
+    class='cmd #{type}'>
+      #{File.basename file}
+  </a>
+EOS
+    end
+
   end
 
 end
