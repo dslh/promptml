5b81b3a02bd08a438030b247a0245ed8e9760cc2
doug <d.lakehammond@gmail.com>
2012-02-25 01:10:56 +1300

    app files now accept erb snippets.

--- lib/promptml/run.rb
+++ lib/promptml/run.rb
@@ -1,3 +1,5 @@
+require 'rubygems'
+require 'erubis'
 require "#{File.dirname __FILE__}/paths.rb"
 
 module PrompTML
@@ -8,18 +10,33 @@ module PrompTML
   class Run
     def call env, args
       cmd = args.shift
-      return usage(cmd) if args.length != 1
+      return usage(cmd) if args.empty?
 
       cwd = Paths.cwd env
       app = Paths.real_path args[0], cwd
       return error "No such file '#{args[1]}'" unless File.exist? app
       return error "Can only run files with the .app extension" unless File.extname(app) == ".app"
 
-      File.read app
+      cmd = args.shift
+      erb = Erubis::Eruby.new File.read(app)
+      erb.result(binding())
     end
     
     def error message
       "<span class='error'><code>run</code> failed: #{message}</span>"
+    end
+
+    def usage cmd
+      <<-EOS
+<pre>
+Usage: #{cmd} cmd.app [args]
+Runs the given .app file. The file will be run through
+an erb interpreter on the server and then appended to
+the page verbatim on the client side. Meta tags or direct
+embedding can be used to initiate javascript on the client
+side.
+</pre>
+EOS
     end
   end
 end
